93e9637b0845c2fe8507c7bc4e9cb86bb937bc23
Jacob <jacob.saini60@gmail.com>
2020-12-18 10:01:33 +1000

    add components, nav bar, add bootstrap

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -8,18 +8,24 @@ import { HomeComponent } from './home/home.component';
 import { DetailComponent } from './detail/detail.component';
 import {ApiServiceService} from './services/api-service.service' 
 import { PayComponent } from './pay/pay.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MenComponent } from './men/men.component';
+import { WomenComponent } from './women/women.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     DetailComponent,
-    PayComponent
+    PayComponent,
+    MenComponent,
+    WomenComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [ApiServiceService],
   bootstrap: [AppComponent]
