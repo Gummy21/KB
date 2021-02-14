a7fb553451fb52bfc791c8d98a100801e281e895
Jacob <jacob.saini60@gmail.com>
2021-02-15 09:38:30 +1000

    aws done

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: "http://kbapi-env-1.eba-mrhmimgh.ap-southeast-2.elasticbeanstalk.com/api"
+  API_URL: "http://kbapi-env-1.eba-mrhmimgh.ap-southeast-2.elasticbeanstalk.com/api/"
   //temp
 };
