70bcab4bd885e8a557c4136fc3a37a049ac67e6f
Jacob <jacob.saini60@gmail.com>
2021-02-19 08:15:58 +1000

    add gitignore

--- api/app.js
+++ api/app.js
@@ -1,11 +1,6 @@
 const express = require("express"),
-      cors    = require("cors"),
       app     = express()
 require('dotenv').config();
-const corsOptions = {
-    origin: 'http://angular-kb-production.s3-website-ap-southeast-2.amazonaws.com',
-    optionsSuccessStatus: 200
-}
 
 db  = require('./models/index')
 Clothes = require("./models/clothes")
