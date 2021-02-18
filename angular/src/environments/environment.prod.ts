013baeea5342b6eb98703f634a7286ad78738490
Jacob <jacob.saini60@gmail.com>
2021-02-19 08:29:26 +1000

    delete and remove env

--- angular/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  API_URL: "http://kbapi-env-1.eba-mrhmimgh.ap-southeast-2.elasticbeanstalk.com/api/"
-  //temp
-};
--- angular/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-
-export const environment = {
-   production: false,
-   API_URL: "http://kbapi-env.eba-mrhmimgh.ap-southeast-2.elasticbeanstalk.com/api/"
-
-};
