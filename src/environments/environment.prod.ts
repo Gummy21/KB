f604e64199143c7e035c8a2423112f88a525f610
Jacob <jacob.saini60@gmail.com>
2021-02-25 09:30:59 +1000

    change git repo location

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  API_URL: "http://149.28.160.43:90/api/"
+  //temp
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+   production: false,
+   API_URL: "http://149.28.160.43:90/api/"
+
+};
